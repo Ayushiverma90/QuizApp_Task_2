ca27f09f444c74d230b0e81cd4e065a59e4803ec
Ayushiverma90 <139061450+Ayushiverma90@users.noreply.github.com>
2023-07-25 13:46:29 +0530

    Add files via upload
    Initial Commit

---
+++ questions.js
@@ -0,0 +1,68 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+     options: [
+             "A. Hyper Text Multi Language",
+             "B. Hyper Text Multiple Language",
+             "C. Hyper Text Markup Language",
+             "D. Home Text Multi Language"
+        ]
+
+    },
+
+{
+    numb: 2,
+    question: "BORDER attributes specifies the?",
+    answer: "A. thickness of the border around the table",
+    options: [
+          "A. thickness of the border around the table",
+          "B. space between two borders",
+          "C. space between two cells",
+          "D. width of the table"
+    ]
+
+},
+
+{
+    numb: 3,
+    question: "Which control is used when the input from the user may extend to several lines?",
+    answer:"A. multiline text area",
+    options:[
+        "A. multiline text area",
+        "B. Check box ",
+        "C. password control",
+        "D. menu control"
+         
+    ]
+
+},
+
+{
+    numb: 4,
+    question: "The item present within the angled brackets in an HTML tag is",
+    answer: "A. identifier",
+    options: [
+        "A. identifier ",
+        "B.  data",
+        "C.  tags",
+        "D.  text "
+    ]
+
+},
+
+{
+    numb: 5,
+    question: "The NOSHADE attribute ______",
+    answer: "B. displays the line in dark gray",
+    options: [
+         "A. displays the example in red",
+         "B. displays the line in dark gray",
+         "C. displays the line in red",
+         "D. defines the thickness of the line",
+    ]
+
+},
+
+]
